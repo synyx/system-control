b6f58b9d9a1abaf2529e4242e4ddd3c60413ad1f
Marc Kannegiesser <kannegiesser@synyx.de>
2016-05-02 14:13:15 +0200

    added extractor that is able to return fields using json-path

---
+++ src/main/java/org/synyx/syscontrol/action/JsonPathResponseExtractor.java
@@ -0,0 +1,42 @@
+package org.synyx.syscontrol.action;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.ResponseExtractor;
+import org.synyx.syscontrol.execution.ExecutionResult;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * @author Marc Kannegiesser - kannegiesser@synyx.de
+ */
+public class JsonPathResponseExtractor implements ResponseExtractor<ExecutionResult> {
+    
+    private final Map<String, String> pathExpressions;
+
+    public JsonPathResponseExtractor(Map<String, String> pathExpressions) {
+        this.pathExpressions = pathExpressions;
+    }
+
+    @Override
+    public ExecutionResult extractData(ClientHttpResponse response) throws IOException {
+        
+        DocumentContext context = JsonPath.parse(response.getBody());
+
+        ExecutionResult.ExecutionResultBuilder builder = ExecutionResult.builder().status(response.getStatusCode());
+        
+        if (response.getStatusCode().is2xxSuccessful()) {
+
+            for (Map.Entry<String, String> pathExpression : pathExpressions.entrySet()) {
+                String result = context.read(pathExpression.getValue(), String.class);
+                builder.data(pathExpression.getKey(), result);
+            }
+            
+        }
+
+        return builder.build();
+
+    }
+}
